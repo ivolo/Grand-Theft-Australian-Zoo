8ec72c17b96fa4c66c1f1c0f4157e2dbcc62b87c
Calvin French-Owen <calvinfo@mit.edu>
2011-04-15 15:35:24 -0400

    Editing src folder

---
+++ src/game_constants/client.py
@@ -0,0 +1,8 @@
+'''
+Created on Apr 15, 2011
+
+@author: calvin
+'''
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
---
+++ src/game_objects/GameObject.py
@@ -0,0 +1,14 @@
+'''
+Created on Apr 15, 2011
+
+@author: calvin
+'''
+import pygame
+
+class GameObject(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pass
+        
+    def update(self):
+        pass
