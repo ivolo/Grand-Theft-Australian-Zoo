53f404e501076ffc15a6af2ff75fb1bcd85ddd80
Caryn Krakauer <caryn@krakauer.org>
2011-04-15 15:46:50 -0400

    main game loop class

---
+++ src/game_objects/gameloop.py
@@ -0,0 +1,20 @@
+'''
+The main gameloop
+'''
+
+class Game:
+    
+    def gameloop():
+        get_input()
+        update_state()
+        draw()
+        return
+    
+    def get_input(self):
+        return
+    
+    def update_state(self):
+        return
+    
+    def draw(self):
+        return
