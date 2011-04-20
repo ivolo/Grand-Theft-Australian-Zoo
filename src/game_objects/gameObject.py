e1526fa45665450ff03dddccb52408ae4e1b99c3
Calvin French-Owen <calvinfo@mit.edu>
2011-04-20 00:41:43 -0400

    added

--- src/game_objects/gameObject.py
+++ src/game_objects/gameObject.py
@@ -7,7 +7,7 @@ import pygame
 
 class GameObject(pygame.sprite.Sprite):
     
-    def __init__(self, image_filename, coordinates):
+    def __init__(self, coordinates):
         super(GameObject, self).__init__()
         
         self.x, self.y = coordinates
--- src/visitor/visitor.py
+++ src/visitor/visitor.py
@@ -6,15 +6,21 @@ tile_size = 32
 class Visitor(GameObject):
     
     def __init__(self, image, x, y, game):
+        
         self.game = game
         self.screen = game.screen
         
-        self.x = x * tile_size
-        self.y = y * tile_size
+
         
         self.image = load_image("visitor.png");
         self.rect = self.image.get_rect()
+        super(Visitor, self).__init__((x, y))
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.rect.left = self.x
+        self.rect.top = self.y
         self.speed = 1
+        
     
     def update(self):
         return
